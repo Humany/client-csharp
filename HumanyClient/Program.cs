6a83b81e9fe3b3ea47340d34d8f71ef036ffcb09
libardo <cristianlibardo@hotmail.com>
2017-01-16 11:19:48 +0100

    Disable statistics by default

--- HumanyClient/Program.cs
+++ HumanyClient/Program.cs
@@ -10,38 +10,38 @@ namespace HumanyClient
 	{
 		static void Main(string[] args)
 		{
-			// Please store this id between calls to humany on a per-user level
-			var userDistinctClientId = Guid.NewGuid().ToString();
-			var client = new ServiceClient("https://help.humany.net/admin-help-en/", client: new ValueProvider(() => userDistinctClientId), enableStatistics: true);
+			var userDistinctClientId = Guid.NewGuid().ToString(); // Please store this id between calls to humany on a per-user level
+			var maintainGuidePopularity = false; // Set this to true in order to maintain popularity on guides
+			var humanyClient = new ServiceClient("https://help.humany.net/admin-help-en/", client: new ValueProvider(() => userDistinctClientId), enableStatistics: maintainGuidePopularity);
 
 			// Prints top-level categories
 			Console.WriteLine("ALL CATEGORIES");
-			var categories = client.GetCategories();
+			var categories = humanyClient.GetCategories();
 			foreach (var c in categories.Children)
 				Console.WriteLine("\t" + c.Name);
 
 			Console.WriteLine("");
 			Console.WriteLine("POPULAR GUIDES");
-			var guides = client.GetGuides();
+			var guides = humanyClient.GetGuides();
 			foreach (var g in guides.Matches)
 				Console.WriteLine("\t" + g.Title);
 
 			Console.WriteLine("");
 			Console.WriteLine("GUIDE #" + guides.Matches[0].Id);
-			var guide = client.GetGuide(guides.Matches[0].Id);
+			var guide = humanyClient.GetGuide(guides.Matches[0].Id);
 			Console.WriteLine(guide.Title);
 			Console.WriteLine(guide.Body.Split('\r', '\n')[0] + "...");
 
 
 			Console.WriteLine("");
 			Console.WriteLine("SEARCH GUIDES");
-			var search = client.GetGuides(new { phrase = "guide" });
+			var search = humanyClient.GetGuides(new { phrase = "guide" });
 			foreach (var g in search.Matches)
 				Console.WriteLine("\t" + g.Title);
 
 			Console.WriteLine("");
 			Console.WriteLine("CATEGORY FACETS");
-			var searchCategories = client.GetCategories(new { phrase = "guide" });
+			var searchCategories = humanyClient.GetCategories(new { phrase = "guide" });
 			foreach (var c in searchCategories.Children.Where(c => c.GuidesCount > 0))
 				Console.WriteLine($"\t{c.Name} ({c.GuidesCount})");
 		}
